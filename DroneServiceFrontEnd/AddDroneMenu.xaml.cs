5b15a50c0db02e8d71b6871e3ebd06a0935fa3b3
P282860 <P282860@RK-F30-610346>
2025-09-16 12:10:46 +0800

    added service Cost double decimal checker

--- DroneServiceFrontEnd/AddDroneMenu.xaml.cs
+++ DroneServiceFrontEnd/AddDroneMenu.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,6 +23,23 @@ namespace DroneServiceFrontEnd
         public AddDroneMenu()
         {
             InitializeComponent();
+        }
+
+        public void DisplayStatus(string msg)
+        {
+            Status2Txt.Text = $"System Status: {msg}";
+        }
+
+        public void ServiceCostTxt_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(ServiceCostTxt.Text, out double value))
+            {
+                ServiceCostTxt.Text = value.ToString("F2");
+            }
+            else
+            {
+                ServiceCostTxt.Text = "0.00";
+            }
         }
     }
 }
